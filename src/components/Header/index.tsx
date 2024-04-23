36a429f3ddf10ce1a2e07adc5ec91e1e3da1ec08
willian <willian.cacoelho@gmail.com>
2024-04-22 22:55:06 -0300

    refactor: change gap of header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,7 +6,7 @@ export const Header = () => {
         <span className="mx-10 text-xl font-semibold">
           <button>WILLCHAT</button>
         </span>
-        <div className="flex h-full gap-4 items-center [&_li]:h-full [&_li]:flex hover:[&_li_button]:text-[#ffffffab] justify-center">
+        <div className="flex h-full gap-6 items-center [&_li]:h-full [&_li]:flex hover:[&_li_button]:text-[#ffffffab] justify-center">
           <li className="h-full flex">
             <button>Home</button>
           </li>
@@ -18,7 +18,7 @@ export const Header = () => {
           </li>
         </div>
       </div>
-      <div className="flex mx-10 gap-4 hover:[&_button]:text-[#ffffffab]">
+      <div className="flex mx-10 gap-6 hover:[&_button]:text-[#ffffffab]">
         <button>Sign In</button>
         <button>Try Free</button>
       </div>
