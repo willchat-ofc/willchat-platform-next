60f78afa06b1585de5558f53c99b2c0bd285db75
willian <willian.cacoelho@gmail.com>
2024-04-25 01:44:41 -0300

    fix: remove warns

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 import { Header } from "@/components/Header";
 import { SignInForm } from "@/components/SignInForm";
-import { SignUpForm } from "@/components/SignUpForm";
 
 export default function Home() {
   return (
-    <main className="h-screen flex flex-col h-screen">
+    <main className="flex flex-col h-screen">
       <Header />
       <section className="w-full h-full flex items-center justify-center">
         <SignInForm />
