185579d841221d4de1870783b3765637dd155d97
willian <willian.cacoelho@gmail.com>
2024-05-03 13:52:51 -0300

    feat: change header buttons hover

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ export const Header = () => {
         <span className="mx-10 text-xl font-semibold">
           <button>WILLCHAT</button>
         </span>
-        <div className="flex h-full gap-6 items-center [&_li]:h-full [&_li]:flex hover:[&_li_button]:text-[#ffffffab] justify-center">
+        <div className="flex h-full gap-6 items-center [&_li]:h-full [&_li]:flex hover:[&_li_button]:text-blue-600 justify-center">
           <li className="h-full flex">
             <button>Home</button>
           </li>
@@ -17,7 +17,7 @@ export const Header = () => {
           </li>
         </div>
       </div>
-      <div className="flex mx-10 gap-6 hover:[&_button]:text-[#ffffffab]">
+      <div className="flex mx-10 gap-6 hover:[&_button]:text-blue-600">
         <button>Sign In</button>
         <button>Try Free</button>
       </div>
