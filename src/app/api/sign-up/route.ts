f1438457ed1c71f19b50fc90cf9d661e150015c4
willian <willian.cacoelho@gmail.com>
2024-06-26 19:45:28 -0300

    feat(sign-up): send the request with valid email and password save tokens in cookies

---
+++ src/app/api/sign-up/route.ts
@@ -0,0 +1,35 @@
+// app/api/route.ts
+import { NextRequest, NextResponse } from "next/server";
+import { cookies } from "next/headers";
+import { ResponseCookie } from "next/dist/compiled/@edge-runtime/cookies";
+
+export async function POST(request: NextRequest) {
+  try {
+    const data = await request.json();
+
+    const response = await fetch("http://localhost:8080/api/auth/sign-up", {
+      body: JSON.stringify(data),
+      method: "POST",
+    });
+
+    const responseBody = await response.json();
+
+    const cookieSettings: Partial<ResponseCookie> = {
+      httpOnly: true,
+      sameSite: "strict",
+      path: "/",
+      secure: process.env.NODE_ENV === "production",
+    };
+
+    if (response.status >= 200 && response.status <= 300) {
+      cookies().set("refreshToken", responseBody.refreshToken, cookieSettings);
+      cookies().set("accessToken", responseBody.accessToken, cookieSettings);
+    }
+
+    return new Response(JSON.stringify(responseBody), {
+      status: response.status,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -13,25 +13,27 @@ import {
 import { Input } from "./ui/input";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
-import { redirect, useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 const formSchema = z.object({
-  name: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-  email: z.string().min(2, {
-    message: "Email must be at least 2 characters.",
-  }),
+  email: z.string().email("Must be a valid email address"),
   password: z.string().min(6, {
     message: "Password must be at least 6 characters.",
   }),
 });
 
 export const SignUpForm = () => {
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
+  const [requestError, setRequestError] = useState<string | null>(null);
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    const response = await fetch("/api/sign-up", {
+      method: "POST",
+      body: JSON.stringify({email: values.email, password: values.password})
+    })
+
+    if (response.status >= 400) {
+      setRequestError((await response.json()).error)
+    }
   }
 
   const router = useRouter();
@@ -39,7 +41,6 @@ export const SignUpForm = () => {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      name: "",
       email: "",
       password: "",
     },
@@ -52,19 +53,6 @@ export const SignUpForm = () => {
           <h1 className="font-bold text-2xl">Sign Up</h1>
           <FormField
             control={form.control}
-            name="name"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Name</FormLabel>
-                <FormControl>
-                  <Input placeholder="Type your name" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
             name="email"
             render={({ field }) => (
               <FormItem>
@@ -72,7 +60,7 @@ export const SignUpForm = () => {
                 <FormControl>
                   <Input placeholder="Type your email" {...field} />
                 </FormControl>
-                <FormMessage />
+                <FormMessage/>
               </FormItem>
             )}
           />
@@ -85,13 +73,14 @@ export const SignUpForm = () => {
                 <FormControl>
                   <Input placeholder="Type your password" {...field} />
                 </FormControl>
-                <FormMessage />
+                <FormMessage color="#FF80AB" />
               </FormItem>
             )}
           />
           <Button type="submit" className="w-full">
             Submit
           </Button>
+          {requestError && <p className="text-center text-[#FF80AB]">{requestError}</p>}
           <p className="w-full flex items-center justify-center gap-1">
             Already have an account?{" "}
             <button
